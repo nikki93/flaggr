60eef5e4edfa6b3c319843e5f0d8b9ea36f4f826
Nikhilesh Sigatapu <s.nikhilesh@gmail.com>
2019-03-18 15:46:54 -0700

    move `applyPlayerWalk` out of 'common.lua'

--- server.lua
+++ server.lua
@@ -206,16 +206,81 @@ function server.update(dt)
         for clientId, player in pairs(share.players) do
             if not player.died then
                 local walk = homes[clientId].walk
-                player.walk = walk
                 if walk then
-                    if player.yDir == 'none' and walk.up and (not walk.down) then
-                        player.yDir = 'up'
+                    player.walk = walk
+
+                    do -- xDir, yDir
+                        if player.yDir == 'none' and walk.up and (not walk.down) then
+                            player.yDir = 'up'
+                        end
+                        if player.yDir == 'none' and walk.down and (not walk.up) then
+                            player.yDir = 'down'
+                        end
                     end
-                    if player.yDir == 'none' and walk.down and (not walk.up) then
-                        player.yDir = 'down'
+
+                    local oldX, oldY = player.x, player.y
+
+                    do -- x
+                        local vx = 0
+                        if walk.left then
+                            vx = vx - PLAYER_X_SPEED
+                        end
+                        if walk.right then
+                            vx = vx + PLAYER_X_SPEED
+                        end
+                        player.x = player.x + vx * dt
+                        player.x = math.max(0, math.min(player.x, W - G))
+                    end
+
+                    do -- y
+                        if player.yDir ~= 'none' then -- Y stepping
+                            if player.yDir == 'up' then
+                                local prevStep = math.floor(player.y / G + 0.9999) -- Which grid step were we at?
+                                player.y = player.y - PLAYER_Y_SPEED * dt
+                                local nextStep = math.floor(player.y / G + 0.9999) -- Which grid step did we get to?
+                                if nextStep ~= prevStep then -- Jumped a step, we're done
+                                    player.yDir = PLAYER_KEY_DELAY
+                                    player.y = (nextStep) * G
+                                end
+                                player.y = math.max(0, math.min(player.y, H - G))
+                                if player.y == 0 or player.y == H - G then
+                                    player.yDir = PLAYER_KEY_DELAY
+                                end
+                            end
+                            if player.yDir == 'down' then
+                                local prevStep = math.floor(player.y / G + 0.1) -- Which grid step were we at?
+                                player.y = player.y + PLAYER_Y_SPEED * dt
+                                local nextStep = math.floor(player.y / G + 0.1) -- Which grid step did we get to?
+                                if nextStep ~= prevStep or nextStep >= H / G then -- Jumped a step, we're done
+                                    player.yDir = PLAYER_KEY_DELAY
+                                    player.y = (nextStep) * G
+                                end
+                                player.y = math.max(0, math.min(player.y, H - G))
+                                if player.y == 0 or player.y == H - G then
+                                    player.yDir = PLAYER_KEY_DELAY
+                                end
+                            end
+
+                            if type(player.yDir) == 'number' then
+                                player.yDir = player.yDir - dt
+                                if player.yDir <= 0 then
+                                    player.yDir = 'none'
+                                end
+                            end
+                        end
+                    end
+
+                    do -- vx, vy
+                        player.vx = (player.x - oldX) / dt
+                        if player.vx ~= 0 then
+                            player.xSetTime = share.time
+                        end
+                        player.vy = (player.y - oldY) / dt
+                        if player.vy ~= 0 then
+                            player.ySetTime = share.time
+                        end
                     end
                 end
-                applyPlayerWalk(share, player, dt)
             end
         end
     end
